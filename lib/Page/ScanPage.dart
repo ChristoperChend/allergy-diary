ae2d584963c9cf2adc1934d7e287bce5efe54db5
ChristoperChend <christoper.chendra15@gmail.com>
2024-06-14 21:40:10 +0700

    add database to scanpage

--- lib/Page/ScanPage.dart
+++ lib/Page/ScanPage.dart
@@ -1,11 +1,13 @@
-// ignore_for_file: camel_case_types, file_names, use_build_context_synchronously
+// ignore_for_file: avoid_print, use_build_context_synchronously, file_names
 
 import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:project/components/ResultScreen.dart';
+import 'package:project/services/allergenService.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
@@ -17,23 +19,39 @@ class ScanPage extends StatefulWidget {
 class _ScanPageState extends State<ScanPage> with WidgetsBindingObserver {
   bool isPermissionGranted = false;
   late final Future<void> future;
-
   CameraController? cameraController;
-  final textRecogniser = TextRecognizer();
+  final textRecognizer = TextRecognizer();
+  final AllergenService allergenService = AllergenService();
+  List<Allergen> allergens = [];
+  List<String> userAllergies = [];
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
     future = requestCameraPermission();
+    fetchAllergens();
+    fetchUserAllergies(); // Fetch user allergies when initializing
   }
 
   @override
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
     stopCamera();
-    textRecogniser.close();
+    textRecognizer.close();
     super.dispose();
+  }
+
+  Future<void> fetchAllergens() async {
+    allergens = await allergenService.getAllergens();
+  }
+
+  Future<void> fetchUserAllergies() async {
+    String userId = 'UY2VuO3XehakiTC8WacnJG2BS1x2';
+    DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(userId).get();
+    if (userDoc.exists) {
+      userAllergies = List<String>.from(userDoc['allergies']);
+    }
   }
 
   @override
@@ -50,81 +68,11 @@ class _ScanPageState extends State<ScanPage> with WidgetsBindingObserver {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: future,
-        builder: (context, snapshot) {
-          return Stack(
-            children: [
-              if (isPermissionGranted)
-                FutureBuilder<List<CameraDescription>>(
-                    future: availableCameras(),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        initCameraController(snapshot.data!);
-                        return Center(
-                          child: CameraPreview(cameraController!),
-                        );
-                      } else {
-                        return const LinearProgressIndicator();
-                      }
-                    }),
-              Scaffold(
-                backgroundColor:
-                    isPermissionGranted ? Colors.transparent : null,
-                body: isPermissionGranted
-                    ? Column(
-                        children: [
-                          Expanded(child: Container()),
-                          Container(
-                            padding: const EdgeInsets.only(bottom: 70),
-                            child: GestureDetector(
-                              onTap: () {
-                                scanImage();
-                              },
-                              // child: const Text('Scan Text')
-                              child: Container(
-                                width: 100,
-                                height: 40,
-                                decoration: BoxDecoration(
-                                  color: const Color.fromRGBO(143, 173, 222, 1),
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: const Center(
-                                  child: Text(
-                                    'Scan Text',
-                                    style: TextStyle(
-                                      fontFamily: 'Outfit',
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    : Center(
-                        child: Container(
-                          padding:
-                              const EdgeInsets.only(left: 24.0, right: 24.0),
-                          child: const Text(
-                            'Camera Permission Denied',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-              ),
-            ],
-          );
-        });
-  }
-
   Future<void> requestCameraPermission() async {
     final status = await Permission.camera.request();
-    isPermissionGranted = status == PermissionStatus.granted;
+    setState(() {
+      isPermissionGranted = status == PermissionStatus.granted;
+    });
   }
 
   void initCameraController(List<CameraDescription> cameras) {
@@ -132,8 +80,7 @@ class _ScanPageState extends State<ScanPage> with WidgetsBindingObserver {
       return;
     }
     CameraDescription? camera;
-    for (var a = 0; a < cameras.length; a++) {
-      final CameraDescription current = cameras[a];
+    for (var current in cameras) {
       if (current.lensDirection == CameraLensDirection.back) {
         camera = current;
         break;
@@ -175,10 +122,17 @@ class _ScanPageState extends State<ScanPage> with WidgetsBindingObserver {
       final pictureFile = await cameraController!.takePicture();
       final file = File(pictureFile.path);
       final inputImage = InputImage.fromFile(file);
-      final recognizerText = await textRecogniser.processImage(inputImage);
+      final recognizedText = await textRecognizer.processImage(inputImage);
+
+      final detectedAllergens = checkAllergens(recognizedText.text);
+
       await navigator.push(
         MaterialPageRoute(
-          builder: (context) => ResultScreen(text: recognizerText.text),
+          builder: (context) => ResultScreen(
+            text: recognizedText.text,
+            detectedAllergens: detectedAllergens,
+            userAllergies: userAllergies,
+          ),
         ),
       );
     } catch (e) {
@@ -188,5 +142,94 @@ class _ScanPageState extends State<ScanPage> with WidgetsBindingObserver {
         ),
       );
     }
+  }
+
+  List<String> checkAllergens(String text) {
+    List<String> detectedAllergens = [];
+    for (var allergen in allergens) {
+      if (text.toLowerCase().contains(allergen.name.toLowerCase())) {
+        detectedAllergens.add(allergen.name);
+      }
+      for (var trigger in allergen.triggers) {
+        if (text.toLowerCase().contains(trigger.toLowerCase())) {
+          detectedAllergens.add(allergen.name);
+        }
+      }
+    }
+    // Debugging print statements to check detected allergens
+    print('Detected Allergens: $detectedAllergens');
+    return detectedAllergens;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: future,
+        builder: (context, snapshot) {
+          return Stack(
+            children: [
+              if (isPermissionGranted)
+                FutureBuilder<List<CameraDescription>>(
+                    future: availableCameras(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        initCameraController(snapshot.data!);
+                        return Center(
+                          child: cameraController != null &&
+                                  cameraController!.value.isInitialized
+                              ? CameraPreview(cameraController!)
+                              : const LinearProgressIndicator(),
+                        );
+                      } else {
+                        return const LinearProgressIndicator();
+                      }
+                    }),
+              Scaffold(
+                backgroundColor:
+                    isPermissionGranted ? Colors.transparent : null,
+                body: isPermissionGranted
+                    ? Column(
+                        children: [
+                          Expanded(child: Container()),
+                          Container(
+                            padding: const EdgeInsets.only(bottom: 70),
+                            child: GestureDetector(
+                              onTap: scanImage,
+                              child: Container(
+                                width: 100,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: const Color.fromRGBO(143, 173, 222, 1),
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    'Scan Text',
+                                    style: TextStyle(
+                                      fontFamily: 'Outfit',
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    : Center(
+                        child: Container(
+                          padding:
+                              const EdgeInsets.only(left: 24.0, right: 24.0),
+                          child: const Text(
+                            'Camera Permission Denied',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+              ),
+            ],
+          );
+        });
   }
 }
